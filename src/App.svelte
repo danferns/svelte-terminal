f4a419b47dd39dc76c92fb3834c7c2a66e6211c8
Valerian Coelho <valerian27012004@gmail.com>
2023-04-17 19:50:07 +0530

    credits Command Added

--- src/App.svelte
+++ src/App.svelte
@@ -29,6 +29,14 @@
 		},
         rick: () => {
 			window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+		},
+        credits: () => {
+			return  ["Site Developed By", 
+                     "-Ethan",
+                     "-Daniel",
+                     "-Valerian",
+                     "-Warren",
+                     "-Atreya"];
 		}
     };
 
